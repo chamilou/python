3eebec6b3cb54edb99765bb01e2d53d54dc2b9e9
chamilou <chamilou@gmail.com>
2017-04-21 10:50:18 +0300

    it' works !!!

--- NewShkat05/src/NewShkat05/views.py
+++ NewShkat05/src/NewShkat05/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 
-def welcome_page (request, template_name="catalog/index.html"):
+def welcome_page (request):
     page_title = 'Musical Instruments and Sheet Music for Musicians'
     doc="HalloMyFriend it's me"
-    return render_to_response(template_name, locals(), context_instance= RequestContext(request))
+    return render_to_response('index.html',locals())
